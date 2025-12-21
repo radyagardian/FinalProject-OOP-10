0fd0c9d762141a64486c427db887b246cfb16e6b
Rafif1901 <mrafifbatubara@gmail.com>
2025-12-21 14:47:46 +0700

    menu, pause, gameOver state

---
+++ Frontend/core/src/main/java/com/finpro/kel10/Frontend/states/DifficultyTransitionState.java
@@ -0,0 +1,67 @@
+package com.finpro.kel10.Frontend.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.finpro.kel10.Frontend.strategies.DifficultyStrategy;
+
+public class DifficultyTransitionState extends GameState {
+    private PlayingState playingState;
+    private DifficultyStrategy newStrategy;
+    private BitmapFont font;
+    private float timer = 2.0f;
+    private String message;
+    private GlyphLayout layout;
+
+    public DifficultyTransitionState(GameStateManager gsm, PlayingState ps, DifficultyStrategy strategy, String message) {
+        super(gsm);
+        this.playingState = ps;
+        this.newStrategy = strategy;
+        this.message = message;
+
+        this.font = new BitmapFont();
+
+        this.font.setColor(Color.WHITE);
+        this.font.getData().setScale(1.2f);
+
+        this.layout = new GlyphLayout();
+    }
+
+    @Override
+    public void handleInput() {}
+
+    @Override
+    public void update(float dt) {
+        timer -= dt;
+        if (timer <= 0) {
+            playingState.setStrategy(newStrategy);
+            gsm.pop();
+        }
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        playingState.render(sb);
+
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+
+        layout.setText(font, message);
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        float x = (w - layout.width) / 2f;
+        float y = (h + layout.height) / 2f;
+
+        font.draw(sb, layout, x, y);
+
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        font.dispose();
+    }
+}
---
+++ Frontend/core/src/main/java/com/finpro/kel10/Frontend/states/GameOverState.java
@@ -0,0 +1,78 @@
+package com.finpro.kel10.Frontend.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.finpro.kel10.Frontend.GameManager;
+
+public class GameOverState extends GameState {
+    private BitmapFont font;
+    private int finalScore;
+    private int totalKills;
+    private GlyphLayout layout;
+
+    public GameOverState(GameStateManager gsm) {
+        super(gsm);
+        font = new BitmapFont();
+        font.getData().setScale(1.5f);
+        font.setColor(Color.WHITE);
+
+        layout = new GlyphLayout();
+
+        this.finalScore = GameManager.getInstance().getScore();
+        this.totalKills = 0;
+    }
+
+    @Override
+    public void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            GameManager.getInstance().resetScore();
+
+            gsm.set(new PlayingState(gsm));
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        float centerY = h / 2f;
+
+        font.setColor(Color.RED);
+        font.getData().setScale(2.5f);
+        drawCenteredText(sb, "GAME OVER", w, centerY + 100);
+
+        font.setColor(Color.WHITE);
+        font.getData().setScale(1.2f);
+        drawCenteredText(sb, "Player: " + GameManager.getInstance().getUsername(), w, centerY + 20);
+        drawCenteredText(sb, "Final Score: " + finalScore, w, centerY - 10);
+        drawCenteredText(sb, "Zombies Killed: " + totalKills, w, centerY - 40);
+
+        font.setColor(Color.YELLOW);
+        drawCenteredText(sb, "Press SPACE to Respawn", w, centerY - 80);
+
+        sb.end();
+    }
+
+    private void drawCenteredText(SpriteBatch sb, String text, float screenWidth, float y) {
+        layout.setText(font, text);
+        float x = (screenWidth - layout.width) / 2f;
+        font.draw(sb, layout, x, y);
+    }
+
+    @Override
+    public void dispose() {
+        font.dispose();
+    }
+}
